9d8d324654f31058aeca879ced685ad9fc3801bb
Graham Cox <graham@grahamcox.co.uk>
2016-04-10 11:40:30 +0100

    Added in support for returning a collection of resources

--- webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/jsonapi/response/JsonApiResponse.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/jsonapi/response/JsonApiResponse.kt
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonInclude
 
 /**
  * Representation of a JSON API Response
+ * @param <T> The type of payload to use for the data
  * @property links The top level links in the response
  * @property data The data in the response
  * @property included Any included resources
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-class JsonApiResponse(val links: JsonApiTopLevelLinks,
-                      val data: JsonApiResource,
+class JsonApiResponse<T>(val links: JsonApiTopLevelLinks,
+                      val data: T,
                       val included: Array<JsonApiResource>? = null)
--- webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/worlds/WorldsController.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/worlds/WorldsController.kt
@@ -13,6 +13,107 @@ import java.time.Clock
 @RestController
 @RequestMapping("/api/worlds")
 open class WorldsController {
+    /**
+     * Get a collection of worlds
+     * @return the worlds
+     */
+    @RequestMapping
+    open fun getWorlds() : JsonApiResponse<Array<JsonApiResource>> {
+        val response = JsonApiResponse(
+                links = JsonApiTopLevelLinks(
+                        self = "/api/worlds"
+                ),
+                data = arrayOf(
+                        JsonApiResource(
+                            type = "world",
+                            id = "abcde",
+                            attributes = mapOf(
+                                    "name" to "Discworld",
+                                    "created" to Clock.systemUTC().instant(),
+                                    "updated" to Clock.systemUTC().instant().plusSeconds(100)
+                            ),
+                            relationships = mapOf(
+                                    "owner" to JsonApiRelationship(
+                                            links = JsonApiRelationshipLinks(
+                                                    self = "/api/worlds/abcde/relationships/owner",
+                                                    related = "/api/worlds/abcde/owner"
+                                            ),
+                                            data = JsonApiResourceIdentifier(
+                                                    type = "user",
+                                                    id = 12345
+                                            )
+                                    )
+                            )
+                        ),
+                        JsonApiResource(
+                                type = "world",
+                                id = "fghij",
+                                attributes = mapOf(
+                                        "name" to "Strata",
+                                        "created" to Clock.systemUTC().instant(),
+                                        "updated" to Clock.systemUTC().instant().plusSeconds(100)
+                                ),
+                                relationships = mapOf(
+                                        "owner" to JsonApiRelationship(
+                                                links = JsonApiRelationshipLinks(
+                                                        self = "/api/worlds/fghij/relationships/owner",
+                                                        related = "/api/worlds/fghij/owner"
+                                                ),
+                                                data = JsonApiResourceIdentifier(
+                                                        type = "user",
+                                                        id = 12345
+                                                )
+                                        )
+                                )
+                        ),
+                        JsonApiResource(
+                                type = "world",
+                                id = "klmno",
+                                attributes = mapOf(
+                                        "name" to "Middle Earth",
+                                        "created" to Clock.systemUTC().instant(),
+                                        "updated" to Clock.systemUTC().instant().plusSeconds(100)
+                                ),
+                                relationships = mapOf(
+                                        "owner" to JsonApiRelationship(
+                                                links = JsonApiRelationshipLinks(
+                                                        self = "/api/worlds/klmno/relationships/owner",
+                                                        related = "/api/worlds/klmno/owner"
+                                                ),
+                                                data = JsonApiResourceIdentifier(
+                                                        type = "user",
+                                                        id = 12346
+                                                )
+                                        )
+                                )
+                        )
+                ),
+                included = arrayOf(
+                        JsonApiResource(
+                                type = "user",
+                                id = 12345,
+                                attributes = mapOf(
+                                        "name" to "Terry Pratchett"
+                                ),
+                                links = JsonApiResourceLinks(
+                                        self = "/api/users/12345"
+                                )
+                        ),
+                        JsonApiResource(
+                                type = "user",
+                                id = 12346,
+                                attributes = mapOf(
+                                        "name" to "J. R. R. Tolkien"
+                                ),
+                                links = JsonApiResourceLinks(
+                                        self = "/api/users/12346"
+                                )
+                        )
+                )
+        )
+
+        return response
+    }
 
     /**
      * Get a single World by ID
@@ -20,7 +121,7 @@ open class WorldsController {
      * @return the world
      */
     @RequestMapping("/{id}")
-    open fun getWorld(@PathVariable("id") id: String) : JsonApiResponse {
+    open fun getWorld(@PathVariable("id") id: String) : JsonApiResponse<JsonApiResource> {
         val response = JsonApiResponse(
                 links = JsonApiTopLevelLinks(
                         self = "/api/worlds/${id}"
