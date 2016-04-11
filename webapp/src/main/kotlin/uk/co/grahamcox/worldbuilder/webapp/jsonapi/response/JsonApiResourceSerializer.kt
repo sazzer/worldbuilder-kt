951a17880d2c015083e4ca5b7a1b89039485932e
Graham Cox <graham@grahamcox.co.uk>
2016-04-11 07:55:27 +0100

    Extracted out a base class from the serializer so that it can be used for collections too

--- webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/jsonapi/response/JsonApiResourceSerializer.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/jsonapi/response/JsonApiResourceSerializer.kt
@@ -14,68 +14,7 @@ class JsonApiResourceSerializer<Input>(private val type: String,
                                        private val attributeGenerator: Map<String, (Input) -> Any>,
                                        private val selfLinkGenerator: (Input) -> String,
                                        private val relatedResources: Map<String, JsonApiRelatedResourceSchema<Input, *>>) :
-        Serializer<Input> {
-
-    /**
-     * Results of building the relationship details of a resource
-     * @property relationships The relationships of the resource
-     * @property included The included resources
-     */
-    data class RelatedResourcesResult(val relationships: Map<String, JsonApiRelationship>,
-                                      val included: List<JsonApiResource>)
-
-    /**
-     * Build the relationship details of a resource
-     * @param input The input object
-     * @param alreadyIncluded The details of already included details
-     * @return the related resource details
-     */
-    private fun buildRelatedResources(input: Input,
-                                      alreadyIncluded: MutableMap<String, MutableSet<Any>>) : RelatedResourcesResult {
-        val relationships = mutableMapOf<String, JsonApiRelationship>()
-        val included = mutableListOf<JsonApiResource>()
-
-        relatedResources.forEach { entry ->
-            val relatedResourceSchema: JsonApiRelatedResourceSchema<Input, Any> = entry.value as JsonApiRelatedResourceSchema<Input, Any>
-            val resource = relatedResourceSchema.resourceExtractor(input)
-
-            if(resource != null) {
-                val id = relatedResourceSchema.idGenerator(resource)
-                val type = relatedResourceSchema.type
-
-                val relationship = JsonApiRelationship(
-                        links = JsonApiRelationshipLinks(
-                                self = relatedResourceSchema.relationshipLinkGenerator?.invoke(input, resource),
-                                related = relatedResourceSchema.relatedLinkGenerator(input, resource)
-                        ),
-                        data = JsonApiResourceIdentifier(
-                                type = type,
-                                id = id
-                        )
-                )
-                relationships.put(entry.key, relationship)
-
-                if (!(alreadyIncluded.get(type)?.contains(id) ?: false)) {
-                    val includedResource = JsonApiResource(
-                            type = type,
-                            id = id,
-                            attributes = relatedResourceSchema.attributeGenerator.mapValues { it.value(resource) },
-                            links = JsonApiResourceLinks(
-                                    self = relatedResourceSchema.selfLinkGenerator(input, resource)
-                            )
-                    )
-
-                    included.add(includedResource)
-                    alreadyIncluded.getOrPut(type) {
-                        mutableSetOf<Any>()
-                    }.add(id)
-                }
-            }
-        }
-
-        return RelatedResourcesResult(relationships = relationships,
-                included = included)
-    }
+        JsonApiSerializerBase<Input>() {
 
     /**
      * Serialize the given Input object to produce a JSON API Response representing the same data
@@ -85,7 +24,7 @@ class JsonApiResourceSerializer<Input>(private val type: String,
     override fun serialize(input: Input): JsonApiResponse<JsonApiResource> {
         val alreadyIncluded = mutableMapOf<String, MutableSet<Any>>()
 
-        val relatedResources = buildRelatedResources(input, alreadyIncluded)
+        val relatedResources = buildRelatedResources(input, relatedResources, alreadyIncluded)
 
         return JsonApiResponse(
                 links = JsonApiTopLevelLinks(
