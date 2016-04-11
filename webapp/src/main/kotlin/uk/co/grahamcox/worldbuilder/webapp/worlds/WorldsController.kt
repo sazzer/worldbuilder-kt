7610f35e49079cc68b90f3f9a33965a7cf685f90
Graham Cox <graham@grahamcox.co.uk>
2016-04-11 09:22:57 +0100

    Decided to move the serializer construction into the controller after all

--- webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/worlds/WorldsController.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/worlds/WorldsController.kt
@@ -12,8 +12,81 @@ import java.time.Clock
  */
 @RestController
 @RequestMapping("/api/worlds")
-open class WorldsController(private val resourceSerializer: JsonApiResourceSerializer<World>,
-                            private val collectionSerializer: JsonApiCollectionSerializer<List<World>, World>) {
+open class WorldsController() {
+    companion object {
+        /** The resource type for the worlds resources */
+        private val RESOURCE_TYPE = "world"
+
+        /** The generator of the ID of a World */
+        private val RESOURCE_ID_GENERATOR = World::id
+
+        /** The attributes of a World */
+        private val RESOURCE_ATTRIBUTES = mapOf(
+                "name" to World::name,
+                "created" to World::created,
+                "updated" to World::updated
+        )
+
+        /** The Self Link of a World */
+        private val RESOURCE_SELF_LINK_GENERATOR = { world: World -> "/api/worlds/${world.id}" }
+
+        /** The relationship representing the owner of a world */
+        private val OWNER_RELATIONSHIP_SCHEMA = JsonApiRelatedResourceSchema<World, User>(
+                type = "user",
+                resourceExtractor = { world -> User(id = 12345, name = "Terry Pratchett") },
+                idGenerator = User::id,
+                relationshipLinkGenerator = { world, user -> "/api/worlds/${world.id}/relationships/owner" },
+                relatedLinkGenerator = { world, user -> "/api/worlds/${world.id}/owner" },
+                selfLinkGenerator = { world, user -> "/api/users/${user.id}" },
+                attributeGenerator = mapOf(
+                        "name" to User::name
+                )
+        )
+
+        /** The relationship representing who last edited a world */
+        private val EDITED_RELATIONSHIP_SCHEMA = JsonApiRelatedResourceSchema<World, User>(
+                type = "user",
+                resourceExtractor = { world -> User(id = 12345, name = "Terry Pratchett") },
+                idGenerator = User::id,
+                relationshipLinkGenerator = { world, user -> "/api/worlds/${world.id}/relationships/edited" },
+                relatedLinkGenerator = { world, user -> "/api/worlds/${world.id}/edited" },
+                selfLinkGenerator = { world, user -> "/api/users/${user.id}" },
+                attributeGenerator = mapOf(
+                        "name" to User::name
+                )
+        )
+
+        /** The complete set of relationships for a world */
+        private val RESOURCE_RELATIONSHIPS = mapOf(
+                "owner" to OWNER_RELATIONSHIP_SCHEMA,
+                "edited" to EDITED_RELATIONSHIP_SCHEMA
+        )
+    }
+
+    /**
+     * Serializer to use for individual resources
+     */
+    private val resourceSerializer = JsonApiResourceSerializer(
+            type = RESOURCE_TYPE,
+            idGenerator = RESOURCE_ID_GENERATOR,
+            attributeGenerator = RESOURCE_ATTRIBUTES,
+            selfLinkGenerator = RESOURCE_SELF_LINK_GENERATOR,
+            relatedResources = RESOURCE_RELATIONSHIPS
+    )
+
+    /**
+     * Serializer to use for collections of resources
+     */
+    private val collectionSerializer = JsonApiCollectionSerializer(
+            type = RESOURCE_TYPE,
+            resourceListGenerator = { worlds: List<World> -> worlds },
+            idGenerator = RESOURCE_ID_GENERATOR,
+            attributeGenerator = RESOURCE_ATTRIBUTES,
+            collectionSelfLinkGenerator = { worlds -> "/api/worlds" },
+            resourceSelfLinkGenerator = RESOURCE_SELF_LINK_GENERATOR,
+            relatedResources = RESOURCE_RELATIONSHIPS
+    )
+
     /**
      * Get a collection of worlds
      * @return the worlds
