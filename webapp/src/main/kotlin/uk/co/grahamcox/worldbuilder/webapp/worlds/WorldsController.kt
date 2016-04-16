7fb0e25b9082443c9253831ffb32faa98cd96547
Graham Cox <graham@grahamcox.co.uk>
2016-04-16 16:28:02 +0100

    Start using the model objects built from the JSON Schema in the responses

--- webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/worlds/WorldsController.kt
+++ webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/worlds/WorldsController.kt
@@ -3,12 +3,10 @@ package uk.co.grahamcox.worldbuilder.webapp.worlds
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import uk.co.grahamcox.worldbuilder.service.ResourceNotFoundException
-import uk.co.grahamcox.worldbuilder.service.users.User
-import uk.co.grahamcox.worldbuilder.service.worlds.World
 import uk.co.grahamcox.worldbuilder.service.worlds.WorldFinder
 import uk.co.grahamcox.worldbuilder.service.worlds.WorldId
-import uk.co.grahamcox.worldbuilder.webapp.jsonapi.response.*
-import uk.co.grahamcox.worldbuilder.webapp.routeTo
+import uk.co.grahamcox.worldbuilder.webapp.api.UserBriefModel
+import uk.co.grahamcox.worldbuilder.webapp.api.WorldModel
 import uk.co.grahamcox.worldbuilder.webapp.swagger.annotations.SwaggerSummary
 import uk.co.grahamcox.worldbuilder.webapp.swagger.annotations.SwaggerTags
 
@@ -16,80 +14,9 @@ import uk.co.grahamcox.worldbuilder.webapp.swagger.annotations.SwaggerTags
  * Controller for interacting with World records
  */
 @RestController
-@RequestMapping(value = "/api/worlds", produces = arrayOf("application/vnd.api+json"))
+@RequestMapping(value = "/api/worlds", produces = arrayOf("application/json"))
 @SwaggerTags(arrayOf("worlds"))
 open class WorldsController(private val worldFinder: WorldFinder) {
-    companion object {
-        /** The resource type for the worlds resources */
-        private val RESOURCE_TYPE = "world"
-
-        /** The generator of the ID of a World */
-        private val RESOURCE_ID_GENERATOR = World::id
-
-        /** The attributes of a World */
-        private val RESOURCE_ATTRIBUTES = mapOf(
-                "name" to World::name,
-                "created" to World::created,
-                "updated" to World::updated
-        )
-
-        /** The Self Link of a World */
-        private val RESOURCE_SELF_LINK_GENERATOR = { world: World -> WorldsController::getWorld.routeTo(world.id.id) }
-
-        /** The relationship representing the owner of a world */
-        private val OWNER_RELATIONSHIP_SCHEMA = JsonApiRelatedResourceSchema<World, User>(
-                type = "user",
-                resourceExtractor = { world -> User(id = world.ownerId, name = "Terry Pratchett") },
-                idGenerator = User::id,
-                relatedLinkGenerator = { world, user -> "/api/worlds/${world.id.id}/owner" },
-                selfLinkGenerator = { world, user -> "/api/users/${user.id.id}" },
-                attributeGenerator = mapOf(
-                        "name" to User::name
-                )
-        )
-
-        /** The complete set of relationships for a world */
-        private val RESOURCE_RELATIONSHIPS = mapOf(
-                "owner" to OWNER_RELATIONSHIP_SCHEMA
-        )
-    }
-
-    /**
-     * Serializer to use for individual resources
-     */
-    private val resourceSerializer = JsonApiResourceSerializer(
-            type = RESOURCE_TYPE,
-            idGenerator = RESOURCE_ID_GENERATOR,
-            attributeGenerator = RESOURCE_ATTRIBUTES,
-            selfLinkGenerator = RESOURCE_SELF_LINK_GENERATOR,
-            relatedResources = RESOURCE_RELATIONSHIPS
-    )
-
-    /**
-     * Serializer to use for collections of resources
-     */
-    private val collectionSerializer = JsonApiCollectionSerializer(
-            type = RESOURCE_TYPE,
-            resourceListGenerator = { worlds: List<World> -> worlds },
-            idGenerator = RESOURCE_ID_GENERATOR,
-            attributeGenerator = RESOURCE_ATTRIBUTES,
-            collectionSelfLinkGenerator = { worlds -> WorldsController::getWorlds.routeTo() },
-            resourceSelfLinkGenerator = RESOURCE_SELF_LINK_GENERATOR,
-            relatedResources = RESOURCE_RELATIONSHIPS
-    )
-
-    /**
-     * Get a collection of worlds
-     * @return the worlds
-     */
-    @RequestMapping(method = arrayOf(RequestMethod.GET))
-    @SwaggerSummary("Get the list of all worlds")
-    open fun getWorlds() : JsonApiResponse<List<JsonApiResource>> {
-        val worlds = worldFinder.findWorlds()
-
-        return collectionSerializer.serialize(worlds)
-    }
-
     /**
      * Get a single World by ID
      * @param id The ID of the World
@@ -97,10 +24,18 @@ open class WorldsController(private val worldFinder: WorldFinder) {
      */
     @RequestMapping(value = "/{id}", method = arrayOf(RequestMethod.GET))
     @SwaggerSummary("Get a single world by ID")
-    open fun getWorld(@PathVariable("id") @SwaggerSummary("The ID of the World") id: String) : JsonApiResponse<JsonApiResource> {
+    open fun getWorld(@PathVariable("id") @SwaggerSummary("The ID of the World") id: String) : WorldModel {
         val world = worldFinder.findWorldById(WorldId(id))
 
-        return resourceSerializer.serialize(world)
+        val result = WorldModel()
+            .withName(world.name)
+            .withCreated(world.created.toString())
+            .withUpdated(world.updated.toString())
+            .withOwner(UserBriefModel()
+                    .withId(world.ownerId.id)
+                    .withName("Terry Pratchett"))
+
+        return result
     }
 
     /**
