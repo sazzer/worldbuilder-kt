3d308fbead5a9d5edeb7198c905b1bc39da4cb4a
Graham Cox <graham@grahamcox.co.uk>
2016-04-16 16:28:45 +0100

    Threw away all of the JSONAPI stuff as I'm no longer using it

--- webapp/src/main/kotlin/uk/co/grahamcox/worldbuilder/webapp/jsonapi/response/JsonApiResourceIdentifier.kt
+++
@@ -1,9 +0,0 @@
-package uk.co.grahamcox.worldbuilder.webapp.jsonapi.response
-
-/**
- * Representation of a Resource Identifier in a JSON API Response
- * @property type The type of the resource
- * @property id The ID of the resource
- */
-class JsonApiResourceIdentifier(val type: String,
-                                val id: Any)
